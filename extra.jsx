b3edfae03dab0cbaea6a8c1d5dfbedd27e9dd390
heena <gheena606@gmail.com>
2025-03-26 14:38:49 +0530

    Add: manage employee template

---
+++ extra.jsx
@@ -0,0 +1,17 @@
+// PORT = 5070
+// LOCAL_DB = mongodb://localhost:27017/hr-ms
+// LOCAL_DB=mongodb+srv://gheena606:wx8SPreCY6f5V6vc@cluster0.suuo2.mongodb.net/
+// LOCAL_DB=mongodb+srv://gheena606:0qXLmf0ySxNztuTn@cluster0.ujkzp.mongodb.net/hr-ms
+// JWT_SECRET = '2b4634be0f804de5ef252d4cb973676f5ad2efa3a45848a8865a0b5c626f5e761d5ec8'
+// EMAIL_USER = anubhav@42works.net
+// EMAIL_PASS = drto vdyk onhc djwx
+
+
+// wx8SPreCY6f5V6vc
+
+
+// 0qXLmf0ySxNztuTn
+// gheena606
+
+
+// localhost:5070/hrms/auth/addNewEmployee
